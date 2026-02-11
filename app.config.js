215a6be80f54766b604da8886e6fa329e7bc6406
Ramesh-ns <ramesh.yenugula@gmail.com>
2026-02-10 20:51:50 -0700

    fix: prod build warning

--- app.config.js
+++ app.config.js
@@ -32,6 +32,9 @@ module.exports = {
       supportsTablet: true,
       bundleIdentifier: 'com.pokertrack.app',
       buildNumber: '1',
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
     },
     android: {
       adaptiveIcon: {
